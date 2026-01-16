f86128654f134a03b961d644edcd90f3bfb4f39f
anasmah4oud <anasmah4oud@gmail.com>
2026-01-16 09:06:54 -0300

    update: fix layout and lc

--- src/components/layout/LogoutButton.tsx
+++ src/components/layout/LogoutButton.tsx
@@ -1,20 +1,21 @@
 'use client'
 
-import { supabase } from '@/lib/supabase/client'
 import { useRouter } from 'next/navigation'
+import { createSupabaseBrowserClient } from '@/lib/supabase/client'
 
 export default function LogoutButton() {
   const router = useRouter()
+  const supabase = createSupabaseBrowserClient()
 
-  const handleLogout = async () => {
+  const logout = async () => {
     await supabase.auth.signOut()
     router.replace('/login')
   }
 
   return (
     <button
-      onClick={handleLogout}
-      className="px-4 py-2 rounded-lg bg-red-600 hover:bg-red-700 transition"
+      onClick={logout}
+      className="bg-red-600 px-4 py-2 rounded-lg"
     >
       تسجيل الخروج
     </button>
