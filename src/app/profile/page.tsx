d9fd23d61dda365a806fe1994c8e9d3498f9f1b1
anasmah4oud <anasmah4oud@gmail.com>
2026-01-22 14:33:13 -1000

    update: fix layout a

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,11 @@
+// app/profile/page.tsx
+
+export default function ProfilePage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-bold">
+        صفحة الملف الشخصي (تحت الإنشاء)
+      </h1>
+    </div>
+  )
+}
