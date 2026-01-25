cd9e878e4a6a91d60c713c2701722fad373f12a6
anasmah4oud <anasmah4oud@gmail.com>
2026-01-25 12:48:15 -1000

    update: fix layout

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -1,6 +1,5 @@
 // app/icon.tsx
 import { ImageResponse } from 'next/og'
-import Image from 'next/image';
 
 export const size = {
   width: 32,
@@ -9,18 +8,34 @@ export const size = {
 
 export const contentType = 'image/png'
 
+// مهم: تشغيله على edge
+export const runtime = 'edge'
+
 export default function Icon() {
   return new ImageResponse(
     (
       <div
- >
-<Image
-  src="/logo.svg"
-  alt="Logo"
-  width={80}
-  height={80}
-  priority
-/>      </div>
+        style={{
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          background: 'transparent',
+        }}
+      >
+        {/* استخدم img عادي بدل next/image */}
+        <img
+          src="/logo.svg"
+          alt="Logo"
+          width={24}
+          height={24}
+          style={{
+            width: 24,
+            height: 24,
+          }}
+        />
+      </div>
     ),
     size
   )
