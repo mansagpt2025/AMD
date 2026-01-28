5d6b80dca445a0cf49e5357aabb385b17b0b0c65
anasmah4oud <anasmah4oud@gmail.com>
2026-01-28 02:27:58 -1000

    update: fix layout

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,7 +1,6 @@
 import Header from '@/components/admin/Header';
 import Sidebar from '@/components/admin/Sidebar';
 import { AuthProvider } from '@/contexts/AuthContext';
-import './layout.css';
 
 export default function AdminLayout({
   children,
